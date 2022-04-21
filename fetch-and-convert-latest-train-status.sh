e788736db4ce1f1f118b76795ea66a3300c5ae3c
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-04-20 20:18:36 -0400

    Bring in JSON to CSV conversion

--- fetch-and-convert-latest-train-status.sh
+++ fetch-and-convert-latest-train-status.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 curl 'https://tsimobile.viarail.ca/data/allData.json' > statuses/json/$timestamp.json
+
+jq '[to_entries[] | {train: .key} + .value]' statuses/json/$timestamp.json | npx json2csv > statuses/csv/$timestamp.csv
