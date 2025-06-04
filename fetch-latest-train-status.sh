c9feb32283bde60883b2bc48e11d3e41de14fdda
Lucas Cherkewski <lucas@lucascherkewski.com>
2025-06-04 17:49:47 -0400

    remove conversion to CSV
    haven't used it, we now have a more robust format for parsing JSON, and it just embedded all
    the time data as JSON anyway--so weren't saving anything

--- fetch-latest-train-status.sh
+++ fetch-latest-train-status.sh
@@ -3,5 +3,3 @@
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 curl 'https://tsimobile.viarail.ca/data/allData.json' > statuses/json/$timestamp.json
-
-jq '[to_entries[] | {train: .key} + .value]' statuses/json/$timestamp.json | npx json2csv > statuses/csv/$timestamp.csv
